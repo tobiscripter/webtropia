575a4a4c2e0e9653968f14f810d1dcc9ad178422
Winkler, Tobias <tobias.winkler@dlr.de>
2022-06-04 02:22:25 +0200

    update

--- mysql/run.sh
+++ mysql/run.sh
@@ -32,7 +32,7 @@ else
 
 	chown -R mysql:mysql /var/lib/mysql
 
-	mysql_install_db --ldata=/var/lib/mysql > /dev/null
+	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
 	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
 		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
